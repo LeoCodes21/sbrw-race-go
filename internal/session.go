ab295093abdbd752c7ddeced8d103c736f900b54
LeoCodes21 <coderleo42+leocodes@gmail.com>
2019-03-23 21:07:47 -0400

    remove old crap

--- internal/session.go
+++ internal/session.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"math"
-	"sort"
 	"sync"
 )
 
@@ -33,41 +31,12 @@ func (s *Session) IncrementSyncCount() {
 	if s.SyncedClients == uint32(s.MaxClients) {
 		s.SyncedClients = 0
 
-		s.SetClientSyncDelays()
 		for _, client := range s.Clients {
 			client.SendSyncResponse()
 		}
 
 		s.SyncCount++
 	}
-}
-
-func (s *Session) SetClientSyncDelays() {
-	if s.ClientCount < 2 {
-		return
-	}
-
-	s.Lock()
-
-	// Sorting clients by ping in ascending order.
-	sortedClients := make([]*Client, len(s.Clients))
-
-	for i, c := range s.Clients {
-		sortedClients[i] = c
-	}
-
-	sort.SliceStable(sortedClients, func(i, j int) bool {
-		return sortedClients[i].Ping < sortedClients[j].Ping
-	})
-
-	// The client with the highest ping should be ignored.
-	// Calculate sync delays for each client based on the client after.
-	for i := 0; i < len(sortedClients) - 1; i++ {
-		sortedClients[i].SyncDelay = (sortedClients[i + 1].Ping - sortedClients[i].Ping) + 10
-		s.Clients[sortedClients[i].SessionSlot].SyncDelay = sortedClients[i].SyncDelay
-	}
-
-	s.Unlock()
 }
 
 func (s *Session) IsAllPlayerInfoBeforeOk() bool {
@@ -78,38 +47,4 @@ func (s *Session) IsAllPlayerInfoBeforeOk() bool {
 	}
 
 	return true
-}
-
-func (s *Session) GetHelloTimeDeviation() uint16 {
-	data := make([]uint16, s.ClientCount)
-	dataIdx := 0
-
-	for _, client := range s.Clients {
-		data[dataIdx] = client.CliHelloTime
-	}
-
-	return uint16(math.Round(float64(StdDev(data))))
-}
-
-func (s *Session) GetPingDeviation() uint16 {
-	data := make([]uint16, s.ClientCount)
-	dataIdx := 0
-
-	for _, client := range s.Clients {
-		data[dataIdx] = client.Ping
-	}
-
-	return uint16(math.Round(float64(StdDev(data))))
-}
-
-func (s *Session) GetPingInfluence(c *Client) uint16 {
-	var result uint16 = 0
-
-	for _, client := range s.Clients {
-		if client.SessionSlot != c.SessionSlot && client.Ping > result {
-			result = client.Ping
-		}
-	}
-
-	return result
 }
